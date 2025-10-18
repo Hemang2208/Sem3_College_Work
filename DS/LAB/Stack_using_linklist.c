6ecfcc8041a6e47fccd0e4071178e7cdae612df5
Kushagra <ay85166@gmail.com>
2025-10-18 19:34:25 +0530

    something new

--- DS/LAB/Stack_using_linklist.c
+++ DS/LAB/Stack_using_linklist.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define max 5
-
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
 typedef struct node{
     int val;
-    struct node* prev;
+    struct node* next;
 }node;
 
 typedef struct Stack{
@@ -12,47 +11,45 @@ typedef struct Stack{
     int count;
 }Stack;
 
-Stack stk={NULL,0}; // initialized an empty Stack named stk with top=NULL
- 
-void push(int val){
-    if (stk.count==max) printf("overflow \n");
+Stack stk = {NULL,0};
+
+void push(int x){
+    node* newnode = malloc(sizeof(node));
+    newnode->val=x;
+    newnode->next=NULL;
+    if (stk.count==max) printf("Overflow\n");
     else{
-        node* newnode=malloc(sizeof(node));        // creating a new node
-        newnode->val=val;                         // assigning value to new node
-        newnode->prev=stk.top;                   // linking new node to it's previous node
-        stk.top=newnode;                        // updating the top 
-        stk.count++; 
-    }                       
+        newnode->next=stk.top->next;
+        stk.top=newnode;
+        stk.count++;
+    }
 }
 
 void pop(){
-    if (stk.top==NULL) printf("underflow \n");
-    else
-    stk.top = stk.top->prev;
-    stk.count--;
+    if (stk.count == 0) printf("Overflow\n");
+    else {
+        stk.top = stk.top->next;
+        stk.count--;
+    }
 }
 
 void peek(){
-    if (stk.top==NULL) printf("Stack empty \n");
-    else printf("%d is the current top\n",stk.top->val);
+    if (stk.top == NULL) printf("Stack empty");
+    else printf("%d \n",stk.top->val);
+    printf("\n");
+
 }
 
 int main(){
-
     for (int i=0 ; i<5 ; i++){
-        push(i); 
-        printf("No. of elements are %d\n",stk.count);
+        push(i);
     }
 
-    push(6); // overflow
-
-    for (int i=0 ; i<5 ; i++){
-        pop(); 
-        printf("No. of elements are %d\n",stk.count);
-    }
-    
     pop();
-     
+    pop();
+
     peek();
 
+    for (int i=1 ; i<=stk.count ; i++ )
+        printf("%d ",stk.top->val);
 }
