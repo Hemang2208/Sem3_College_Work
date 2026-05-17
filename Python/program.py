2c1b169ab485586929b2d2bdd95952e82381d915
Kushagra <ay85166@gmail.com>
2026-05-18 01:21:47 +0530

    pta nahi kya kardiya

---
+++ Python/program.py
@@ -0,0 +1,10 @@
+n = 5
+# Upper half
+for i in range(1, n + 1):
+    print(" " * (n - i) + " ".join(str(j) for j in range(1, i + 1)))
+
+# Lower half
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + " ".join(str(j) for j in range(1, i + 1)))
+
+print()
